caf7b11ca265163ab6f16503675d1602001056ca
HP <farishtasulaiman@gmail.com>
2024-10-17 10:41:16 +0200

    Add project files.

---
+++ FitTack/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTack
+{
+    public abstract class Person
+    {
+
+        // property
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+        // konstruktor
+        public Person(string UserName, string PassWord)
+        {
+            this.UserName = UserName;
+            this.PassWord = PassWord;
+
+        }
+
+
+        // metod 
+        public abstract string SignIn();
+
+    }
+}
+
