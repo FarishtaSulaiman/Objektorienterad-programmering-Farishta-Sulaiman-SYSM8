0cc42d9e0aa789f18d3a45b1cec835ae5f33c724
HP <farishtasulaiman@gmail.com>
2024-10-21 22:15:31 +0200

    skapat windowfactory class med metoder för att stänga och öppna olika fönster

---
+++ FitTack/WindowFactory.cs
@@ -0,0 +1,34 @@
+﻿using FitTack.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTack
+{
+    public class WindowFactory : IWindowFactory
+    {
+        // Öppna MainWindow
+        public void ShowMainWindow()
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        // Öppna RegisterWindow
+        public void ShowRegisterWindow()
+        {
+            var registerWindow = new RegisterWindow();
+            registerWindow.Show();
+        }
+
+        // Stäng nuvarande fönster
+        public void CloseWindow()
+        {
+            // Stänger det aktuella öppna fönstret
+            Application.Current.Windows[0]?.Close();
+        }
+    }
+}
+}
